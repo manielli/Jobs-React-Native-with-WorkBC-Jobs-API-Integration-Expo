d68dc859cadb11f1210525d348dbba2addb7569d
Mani Jafari <manielli@gmail.com>
2019-03-20 15:46:02 -0700

    Added a review tab nested to the main createBottomTabNavigator nested in the main of the first createBottomTabNavigator

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { 
+  createAppContainer, 
+  createBottomTabNavigator, 
+  createStackNavigator 
+} from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 const MainNavigator = createBottomTabNavigator({
   welcome: { screen: WelcomeScreen },
@@ -13,7 +19,13 @@ const MainNavigator = createBottomTabNavigator({
   main: {
     screen: createBottomTabNavigator({
       map: { screen: MapScreen },
-      deck: { screen: DeckScreen }
+      deck: { screen: DeckScreen },
+      review: {
+        screen: createStackNavigator({
+          review: { screen : ReviewScreen },
+          settings: { screen: SettingsScreen }
+        })
+      }
     })
   }  
 });
