9af2f5607ee660f4576a8bdd506867cbd56388ee
Mani Jafari <manielli@gmail.com>
2019-03-20 16:10:39 -0700

    Added header button to the static navigationOptions of Review Screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends React.Component {
     static navigationOptions = {
         title: 'Review Jobs',
         headerRight: (
-            <Text>Go Right</Text>
+            <Button title="Settings" onPress={() => {}} />
         )
     }
 
