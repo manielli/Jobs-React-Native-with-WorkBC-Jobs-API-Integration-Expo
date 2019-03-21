611720e7173bacbaa82a9b9cd4016e23799ba2a6
Mani Jafari <manielli@gmail.com>
2019-03-20 17:04:27 -0700

    added some hardcoded data prop for the Slides component in the WelcomeScreen screen component

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Slides from '../components/Slides';
 
+const SLIDE_DATA = [
+    { text: 'Welcome to this JobApp' },
+    { text: 'Use this app to find a job of your dreams' },
+    { text: 'Set your location, then swipe away' }
+];
+
 export default class WelcomeScreen extends React.Component {
     render() {
         return (
-            <Slides />
+            <Slides data={SLIDE_DATA} />
         );
     }
 }
