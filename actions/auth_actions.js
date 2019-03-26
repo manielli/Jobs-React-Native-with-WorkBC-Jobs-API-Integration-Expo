409aa84215bf39e3df27fc2fb8d26a7a5de1491b
Mani Jafari <manielli@gmail.com>
2019-03-26 15:15:10 -0700

    Imported Facebook from expo and created a helper method for doFacebookLogin() to handle the login of the user using facebook auth

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,5 +1,6 @@
 import { FACEBOOK_LOGIN_SUCCESS } from './types';
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 // How to use AsyncStorage
 // AsyncStorage.setItem('fb_token', token);
@@ -9,8 +10,13 @@ export const facebookLogin = () => async dispatch => {
     let token = await AsyncStorage.getItem('fb_token');
     if (token) {
         // Dispatch an action saying FB login is done
-        
+        dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
         // Start up FB Login process
+        doFacebookLogin();
     }
+};
+
+const doFacebookLogin = () => {
+    
 };
