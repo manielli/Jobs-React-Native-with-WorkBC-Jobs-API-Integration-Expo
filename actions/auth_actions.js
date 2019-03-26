0ffd8b4777f68398e2ec19a07b53d7369ea397b1
Mani Jafari <manielli@gmail.com>
2019-03-26 15:44:09 -0700

    Added FACEBOOK_LOGIN_FAIL action type constant in types.js file and improved the doFacebookLogin() helper function

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,4 +1,7 @@
-import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import { 
+    FACEBOOK_LOGIN_SUCCESS,
+    FACEBOOK_LOGIN_FAIL 
+} from './types';
 import { AsyncStorage } from 'react-native';
 import { Facebook } from 'expo';
 
@@ -13,12 +16,16 @@ export const facebookLogin = () => async dispatch => {
         dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
         // Start up FB Login process
-        doFacebookLogin();
+        doFacebookLogin(dispatch);
     }
 };
 
-const doFacebookLogin = async () => {
-    let result = await Facebook.logInWithReadPermissionsAsync('2422058358062134', {
+const doFacebookLogin = async (dispatch) => {
+    let { type, token } = await Facebook.logInWithReadPermissionsAsync('2422058358062134', {
         permission: ['public_profile'];
     });
+
+    if (type === 'cancel') {
+        return dispatch({ type: FACEBOOK_LOGIN_FAIL})
+    }
 };
