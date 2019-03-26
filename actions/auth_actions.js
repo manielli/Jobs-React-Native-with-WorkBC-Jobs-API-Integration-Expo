9572383ec12c072f564cacb066200bbfce4f4ad9
Mani Jafari <manielli@gmail.com>
2019-03-26 12:14:50 -0700

    Created a types.js file to store action types as constants and created auth_actions.js file to hold the action creators related to authentication or auth reducer

---
+++ actions/auth_actions.js
@@ -0,0 +1,10 @@
+import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import { AsyncStorage } from 'react-native';
+
+// How to use AsyncStorage
+// AsyncStorage.setItem('fb_token', token);
+// AsyncStorage.getItem('fb_token');
+
+export const facebookLogin = () => {
+    
+}
