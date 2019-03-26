10302201ab426dbddfcf10467fd592f10819d73c
Mani Jafari <manielli@gmail.com>
2019-03-26 15:02:21 -0700

    Refactoring to use shorthand async/await syntax

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -5,13 +5,12 @@ import { AsyncStorage } from 'react-native';
 // AsyncStorage.setItem('fb_token', token);
 // AsyncStorage.getItem('fb_token');
 
-export const facebookLogin = () => 
-    async (dispatch) => {
-        let token = await AsyncStorage.getItem('fb_token');
-        if (token) {
-            // Dispatch an action saying FB login is done
-            
-        } else {
-            // Start up FB Login process
-        }
-    };
+export const facebookLogin = () => async dispatch => {
+    let token = await AsyncStorage.getItem('fb_token');
+    if (token) {
+        // Dispatch an action saying FB login is done
+        
+    } else {
+        // Start up FB Login process
+    }
+};
