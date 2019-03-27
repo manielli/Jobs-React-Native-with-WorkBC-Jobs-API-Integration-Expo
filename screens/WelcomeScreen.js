68e91902f1de2affb9017fc706437e633abf1308
Mani Jafari <manielli@gmail.com>
2019-03-27 14:22:39 -0700

    Installed lodash using yarn add, added an asynchronous componentWillMount() lifecycle method to get the token in the state using AsyncStorage

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slides from '../components/Slides';
+import { AppLoading } from 'expo';
 
 const SLIDE_DATA = [
     { text: 'Welcome to this JobApp', color: '#03A9F4' },
@@ -8,11 +9,23 @@ const SLIDE_DATA = [
 ];
 
 export default class WelcomeScreen extends React.Component {
+    state = { token: null }
+
+    async componentWillMount() {
+        let token = await AsyncStorage.getItem('fb_token');
+
+        this.setState({ token });
+    }
+
     onSlidesComplete = () => {
         this.props.navigation.navigate('auth');
     }
 
     render() {
+        if (_.isNull(this.state.token)) {
+            return <AppLoading />
+        }
+
         return (
             <Slides 
                 data={SLIDE_DATA} 
