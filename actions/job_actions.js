99fa7ebe3e718cb55403fb84384807fe1eb4f082
Mani Jafari <manielli@gmail.com>
2019-03-28 14:50:46 -0700

    Installed latlng-to-zip package using yarn add, added reverseGeocode query inside the fetchJobs action creator

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
 import { FETCH_JOBS } from './types';
 
-export const fetchJobs = () => {
-    
+export const fetchJobs = (region) => {
+    return async function(dispatch) {
+        try {
+            let zip = await reverseGeocode(region);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
 };
