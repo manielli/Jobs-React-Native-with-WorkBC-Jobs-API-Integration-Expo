eb1416765769850e19a508b320fc4aa01d8db663
Mani Jafari <manielli@gmail.com>
2019-03-28 16:35:56 -0700

    buildJobUrl helper function in job_actions.js action creator

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
 import qs from 'qs';
 import { FETCH_JOBS } from './types';
+import { constant } from '../constants';
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 
 const JOB_QUERY_PARAMS = {
     publisher: '4201738803816157',
@@ -12,12 +15,20 @@ const JOB_QUERY_PARAMS = {
     q: 'javascript'
 };
 
+const JOB_QUERY_PARAMS2 = {
+    apiKey: constant.apiKey
+};
+
+const buildJobsUrl = (zip) => {
+    const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+    return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = (region) => {
     return async function(dispatch) {
         try {
             let zip = await reverseGeocode(region);
-            console.log(zip);
-
+            
         } catch (error) {
             console.error(error);
         }
