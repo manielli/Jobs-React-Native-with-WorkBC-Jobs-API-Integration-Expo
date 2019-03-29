c187b402b2779e625e7900a1d8d84d5662257a31
Mani Jafari <manielli@gmail.com>
2019-03-28 17:09:15 -0700

    Called the action creator fetchJobs in MapScreen with wiring it up to a Button component from react-native-elements on MapView component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
 
 import * as actions from '../actions';
 
@@ -25,6 +26,10 @@ class MapScreen extends React.Component {
         this.setState({ region });
     }
 
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region);
+    }
+
     render() {
         if (!this.state.mapLoaded) {
             return (
@@ -45,9 +50,27 @@ class MapScreen extends React.Component {
                     style={{ flex: 1 }} 
                     onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+                <View style={styles.buttonContainer} >
+                    <Button 
+                        large
+                        title='Search This Area'
+                        backgroundColor='#009688'
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPress} 
+                    />
+                </View>
             </View>
         );
     }
 }
+
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0
+    }
+};
 
 export default connect(null, actions)(MapScreen);
