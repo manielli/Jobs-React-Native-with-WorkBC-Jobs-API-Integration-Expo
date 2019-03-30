0e2981df49f56754a5f3f02ee3a38362e423b0cb
Mani Jafari <manielli@gmail.com>
2019-03-29 18:56:53 -0700

    Wired up DeckScreen to our redux store using connect helper from react-redux

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { jobsFetch } from '../actions';
 
-export default class DeckScreen extends React.Component {
+class DeckScreen extends React.Component {
     render() {
         return (
             <View>
@@ -15,3 +17,9 @@ export default class DeckScreen extends React.Component {
         );
     }
 }
+
+function mapStateToProps({ jobs }) {
+    return { jobs: jobs.result };
+}
+
+export default connect(mapStateToProps, { jobsFetch })(DeckScreen);
