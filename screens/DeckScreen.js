83a84998f473e6a380577cda9b8b53d4fa88b051
Mani Jafari <manielli@gmail.com>
2019-04-01 18:38:34 -0700

    Added the MapView to the renderCard helper method return

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView } from 'expo';
 import { Card, Button } from 'react-native-elements';
@@ -8,9 +8,23 @@ import { fetchJobs } from '../actions';
 
 class DeckScreen extends React.Component {
     renderCard(job) {
+        const initialRegion = {
+            longitude: job.longitude,
+            latitude: job.latitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.02
+        };
+
         return (
            <Card title={job.jobTitle} >
-            <MapView />
+                <View style={{ height: 300 }} >
+                    <MapView 
+                        scrollEnabled={false}
+                        style={{ flex: 1 }}
+                        cacheEnabled={Platform.OS === 'android' ? true : false}
+                        initialRegion={initialRegion}
+                    />
+                </View>
                 <View style={styles.detailWrapper} >
                     <Text>{job.employerName}</Text>
                     <Text>{job.postedDate.split('T')[0]}</Text>
